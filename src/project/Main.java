2a519a4c758792fc384f290aabbb00e8cff070ee
MikhayahIV <sasha.vessa@gmail.com>
2026-03-03 06:41:46 -0300

    feat: adiciona a main para iniciar o projeto utilizando o dao

---
+++ src/project/Main.java
@@ -0,0 +1,17 @@
+package project;
+
+public class Main {
+    public static  void inicialMensagem(){
+        System.out.println("Digite o numero da operacao que deseja realizar: " +
+                "\n1 - Cadastrar produto" +
+                "\n2 - listar todos produtos" +
+                "\n3 - verificar um produto pelo id" +
+                "\n4 - atualizar um produto" +
+                "\n5 - deletar um produto" +
+                "\n6 - sair do programa");
+    }
+    static void main() {
+
+
+    }
+}
