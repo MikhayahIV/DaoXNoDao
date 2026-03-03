56f2f4e4b31c6d08638109943896788701446114
MikhayahIV <sasha.vessa@gmail.com>
2026-03-03 06:42:27 -0300

    refactor: inicio do refatoramento da main sem o dao

--- src/semDao/Main.java
+++ src/semDao/Main.java
@@ -6,20 +6,68 @@ import java.util.UUID;
 
 public class Main {
     static void main() {
-        try {
+
+        int opcao = 0;
+        Scanner scanner = new Scanner(System.in);
+        do{
+
+            try {
+                System.out.println("Digite o numero da operacao que deseja realizar: " +
+                        "\n1 - Cadastrar Produto" +
+                        "\n2 - Listar todos os produtos" +
+                        "\n3 - buscar um produto por id" +
+                        "\n4 - alterar informacoes de um produto" +
+                        "\n5 - deletar produto" +
+                        "\n6 - fechar programa");
+
+                int escolha = scanner.nextInt();
+                scanner.nextLine();
+                opcao = escolha;
+
+            } catch (IllegalArgumentException ex){
+                System.out.println("ERRO: Por favor digite apenas o número da operaçao que deseja realizar");
+            }
+
+            switch (opcao){
+                case 1:
+                    try{
+                        System.out.println("---- CADASTRAR PRODUTO ----");
+                        System.out.print("Digite o nome do produto: ");
+                        String name = scanner.nextLine();
+                        System.out.print("Digite a descricao do produto: ");
+                        String description = scanner.nextLine();
+                        System.out.print("Digite o preco do produto: ");
+                        double price = scanner.nextDouble();
+                        System.out.print("Digite a quantidade do produto: ");
+                        int quantity = scanner.nextInt();
+                        Produtos prod = new Produtos(name,description,quantity,price);
+                        prod.cadastarUsuario(prod);
+                        System.out.println("Produto cadastrado com sucesso.");
+
+                    } catch (SQLException | ClassNotFoundException ex){
+                        System.out.println("ERROR: "+ex);
+                    }break;
+                case 2:
+                    break;
+            }
+        }while (opcao !=6);
+
+        //vai ficar com muitas linha esse krl
+
+/*        try {
             Scanner scanner = new Scanner(System.in);
             Produtos prod = new Produtos();
             System.out.println(prod.listarTodosProdutos());
 
-/*
+
             System.out.print("Digite o id do produto: ");
             String id = scanner.nextLine();
             UUID uid = UUID.fromString(id);
-            System.out.println(prod.listarProdPorId(uid));*/
+            System.out.println(prod.listarProdPorId(uid));
 
 
 
-/*          System.out.print("Digite o id do produto: ");
+          System.out.print("Digite o id do produto: ");
             String entradaId = scanner.nextLine();
             UUID id = UUID.fromString(entradaId);
             System.out.print("Digite o novo nome do produto: ");
@@ -31,14 +79,14 @@ public class Main {
             System.out.print("Digite o valor do produto: ");
             double price = scanner.nextDouble();
             Produtos prod = new Produtos(name,description,quantity,price);
-            prod.atualizarProdutoPorId(id,prod);*/
+            prod.atualizarProdutoPorId(id,prod);
 
-/*          System.out.print("Digite o id do produto que deseja deletar:  ");
+          System.out.print("Digite o id do produto que deseja deletar:  ");
             String entrada = scanner.nextLine();
             UUID id = UUID.fromString(entrada);
             Produtos prod = new Produtos();
-            prod.deletarProdutoPorId(id);*/
-/*          System.out.print("Digite o nome do produto: ");
+            prod.deletarProdutoPorId(id);
+          System.out.print("Digite o nome do produto: ");
             String name = scanner.nextLine();
             System.out.print("Digite a descricao do produto: ");
             String description = scanner.nextLine();
@@ -48,10 +96,10 @@ public class Main {
             Double price = scanner.nextDouble();
             Produtos prod = new Produtos(name,description,quantity,price);
             prod.cadastarUsuario(prod);
-            System.out.println("Produto cadastrado com sucesso.");*/
+            System.out.println("Produto cadastrado com sucesso.");
 
         } catch (SQLException | ClassNotFoundException ex){
             System.out.println("ERROR: "+ex);
-        }
+        }*/
     }
 }
